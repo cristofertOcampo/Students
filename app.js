30bed7523ffa3a2e1cfd763e9ab612e2ea5d5092
Cristofert Ocampo <ocampocristofert@gmail.com>
2022-10-20 16:32:46 -0500

    js

---
+++ app.js
@@ -0,0 +1,15 @@
+
+const age = document.getElementById("txtAge");
+const kilometres = document.getElementById("txtKilometres");
+const send = document.getElementById("btnSend");
+const paragraph = document.getElementById("paragraphGroup");
+
+let daytime = [];
+let afternoon = [];
+let night = [];
+console.log(night.length);
+
+send.addEventListener('click', ()=>{
+  console.log(typeof age.value ); 
+
+});
